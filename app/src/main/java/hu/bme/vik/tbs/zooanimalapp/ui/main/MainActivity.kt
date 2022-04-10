de929c3c5a63cd6b94f40d52ab6c03c4176e0369
Stella Tóth-Baranyi <stellasipi@gmail.com>
2022-04-10 23:36:24 +0200

    Add main activity

--- app/src/main/java/hu/bme/vik/tbs/zooanimalapp/ui/main/MainActivity.kt
+++ app/src/main/java/hu/bme/vik/tbs/zooanimalapp/ui/main/MainActivity.kt
@@ -1,4 +1,12 @@
 package hu.bme.vik.tbs.zooanimalapp.ui.main
 
-class MainActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import hu.bme.vik.tbs.zooanimalapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 }
