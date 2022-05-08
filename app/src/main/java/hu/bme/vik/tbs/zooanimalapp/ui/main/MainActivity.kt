f2fc1263f7536408e00fc149125088ed7d014c4d
Stella Tóth-Baranyi <stellasipi@gmail.com>
2022-05-08 23:34:49 +0200

    Reset some defaults

--- app/src/main/java/hu/bme/vik/tbs/zooanimalapp/ui/main/MainActivity.kt
+++ app/src/main/java/hu/bme/vik/tbs/zooanimalapp/ui/main/MainActivity.kt
@@ -30,28 +30,8 @@ class MainActivity : ComponentActivity() {
             CompositionLocalProvider(LocalCoilImageLoader provides viewModel.imageLoader) {
                 ZooAnimalAppTheme {
                     ZooAnimalMainScreen()
-//                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colors.background
-//                ) {
-//                    Greeting("Android")
-//                }
                 }
             }
         }
     }
 }
-
-//@Composable
-//fun Greeting(name: String) {
-//    Text(text = "Hello $name!")
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun DefaultPreview() {
-//    ZooAnimalAppTheme {
-//        Greeting("Android")
-//    }
-//}
